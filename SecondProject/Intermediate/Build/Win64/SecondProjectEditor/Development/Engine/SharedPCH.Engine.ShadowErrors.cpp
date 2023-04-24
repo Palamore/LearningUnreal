8ff3cd18fe98380ed7ac986baed8f9ef3600106b
gaminem9 <gaminem9@gmail.com>
2023-04-24 21:47:15 +0900

    [Client] 디폴트 생성된 프로젝트, gitignore 업로드

---
+++ SecondProject/Intermediate/Build/Win64/SecondProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,1 @@
+#include "C:/UnrealEngine4/LearningUnreal/SecondProject/Intermediate/Build/Win64/SecondProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ SecondProject/Intermediate/Build/Win64/UE4Editor/Inc/SecondProject/SecondProjectGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef SECONDPROJECT_SecondProjectGameModeBase_generated_h
+#error "SecondProjectGameModeBase.generated.h already included, missing '#pragma once' in SecondProjectGameModeBase.h"
+#endif
+#define SECONDPROJECT_SecondProjectGameModeBase_generated_h
+
+#define SecondProject_Source_SecondProject_SecondProjectGameModeBase_h_15_SPARSE_DATA
+#define SecondProject_Source_SecondProject_SecondProjectGameModeBase_h_15_RPC_WRAPPERS
+#define SecondProject_Source_SecondProject_SecondProjectGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define SecondProject_Source_SecondProject_SecondProjectGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesASecondProjectGameModeBase(); \
+	friend struct Z_Construct_UClass_ASecondProjectGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ASecondProjectGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/SecondProject"), NO_API) \
+	DECLARE_SERIALIZER(ASecondProjectGameModeBase)
+
+
+#define SecondProject_Source_SecondProject_SecondProjectGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesASecondProjectGameModeBase(); \
+	friend struct Z_Construct_UClass_ASecondProjectGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ASecondProjectGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/SecondProject"), NO_API) \
+	DECLARE_SERIALIZER(ASecondProjectGameModeBase)
+
+
+#define SecondProject_Source_SecondProject_SecondProjectGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ASecondProjectGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ASecondProjectGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ASecondProjectGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ASecondProjectGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ASecondProjectGameModeBase(ASecondProjectGameModeBase&&); \
+	NO_API ASecondProjectGameModeBase(const ASecondProjectGameModeBase&); \
+public:
+
+
+#define SecondProject_Source_SecondProject_SecondProjectGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ASecondProjectGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ASecondProjectGameModeBase(ASecondProjectGameModeBase&&); \
+	NO_API ASecondProjectGameModeBase(const ASecondProjectGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ASecondProjectGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ASecondProjectGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ASecondProjectGameModeBase)
+
+
+#define SecondProject_Source_SecondProject_SecondProjectGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define SecondProject_Source_SecondProject_SecondProjectGameModeBase_h_12_PROLOG
+#define SecondProject_Source_SecondProject_SecondProjectGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	SecondProject_Source_SecondProject_SecondProjectGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	SecondProject_Source_SecondProject_SecondProjectGameModeBase_h_15_SPARSE_DATA \
+	SecondProject_Source_SecondProject_SecondProjectGameModeBase_h_15_RPC_WRAPPERS \
+	SecondProject_Source_SecondProject_SecondProjectGameModeBase_h_15_INCLASS \
+	SecondProject_Source_SecondProject_SecondProjectGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define SecondProject_Source_SecondProject_SecondProjectGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	SecondProject_Source_SecondProject_SecondProjectGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	SecondProject_Source_SecondProject_SecondProjectGameModeBase_h_15_SPARSE_DATA \
+	SecondProject_Source_SecondProject_SecondProjectGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	SecondProject_Source_SecondProject_SecondProjectGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	SecondProject_Source_SecondProject_SecondProjectGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> SECONDPROJECT_API UClass* StaticClass<class ASecondProjectGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID SecondProject_Source_SecondProject_SecondProjectGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
